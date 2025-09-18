88b9f0263a333eb168772a9e52a6db5a342680d8
Juan Galvis <Galvis>
2025-09-18 11:29:40 -0500

    Add Movie Class

---
+++ src/jcgc/play/content/Movie.java
@@ -0,0 +1,12 @@
+package jcgc.play.content;
+
+public class Movie {
+
+    public String title;
+    public String description;
+    public int duration;
+    public String genre;
+    public int releaseYear;
+    public double score;
+    public boolean available;
+}
