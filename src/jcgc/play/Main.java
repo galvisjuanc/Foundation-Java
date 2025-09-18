7ee2f14c018b0fecaffb50451e9bfd458e79cc78
Juan Galvis <Galvis>
2025-09-18 11:39:59 -0500

    Movie class used in Main class

--- src/jcgc/play/Main.java
+++ src/jcgc/play/Main.java
@@ -1,5 +1,7 @@
 package jcgc.play;
 
+import jcgc.play.content.Movie;
+
 import java.util.Scanner;
 
 public class Main {
@@ -7,20 +9,14 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("JCGC Play 😎");
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Cuál es tu nombre?: ");
+        Movie movie = new Movie();
+        movie.title = "El señor de los anillos";
+        movie.description = "Mi preciosoooooo!";
+        movie.releaseYear = 2001;
+        movie.genre = "Fantasia";
+        movie.giveScore(4.7);
 
-        String name = sc.nextLine();
-        System.out.println("Hola " + name + ", esto es jcgc play!");
-
-        System.out.println(name + ", ¿cuantos años tienes?");
-        int age = sc.nextInt();
-
-        if (age >= 18) {
-            System.out.println(name + ", puedes ver contenido de adultos por ser mayor de edad");
-        } else {
-            System.out.println("No puedes ver este contenido. Tienes prohibido el acceso");
-        }
+        System.out.println(movie.getTechnicalDatasheet());
 
     }
 }
