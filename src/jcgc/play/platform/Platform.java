f9e05ab089901d5cf53aa513c4a6afe33cf71fdc
Juan Galvis <Galvis>
2025-10-16 13:30:20 -0500

    New constructor and void method

--- src/jcgc/play/platform/Platform.java
+++ src/jcgc/play/platform/Platform.java
@@ -7,4 +7,12 @@ import java.util.List;
 public class Platform {
     private String name;
     private List<Movie> movies;
+
+    public Platform(String name) {
+        this.name = name;
+    }
+
+    public void addMovie(Movie movie) {
+        this.movies.add(movie);
+    }
 }
