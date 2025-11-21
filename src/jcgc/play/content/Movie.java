1f1cd89f139fd4f074c2b44d400c6e445565ff8c
Juan Galvis <Galvis>
2025-11-21 10:38:56 -0500

    Modify the play method to be abstract in the movie class

--- src/jcgc/play/content/Movie.java
+++ src/jcgc/play/content/Movie.java
@@ -4,4 +4,9 @@ public class Movie extends Content{
     public Movie(String title, int duration, Genre genre, double score) {
         super(title, duration, genre, score);
     }
+
+    @Override
+    public void play() {
+        System.out.println("Reproduciendo la pelicula: " + getTitle());
+    }
 }
